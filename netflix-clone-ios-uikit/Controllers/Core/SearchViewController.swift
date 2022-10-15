7cf215fe4130b19d16ef247fa73bc138da60b4d4
Samet Koyuncu <sametkoyuncu@live.com>
2022-10-15 19:22:03 +0300

    Complete search screen

---
+++ netflix-clone-ios-uikit/Controllers/Core/SearchViewController.swift
@@ -0,0 +1,120 @@
+//
+//  SearchViewController.swift
+//  netflix-clone-ios-uikit
+//
+//  Created by Samet Koyuncu on 26.09.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    private var titles: [Title] = [Title]()
+    
+    private let discoverTable: UITableView = {
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier )
+        return table
+    }()
+    
+    // search controller
+    private let searchController: UISearchController = {
+        let controller = UISearchController(searchResultsController: SearchResultsViewController())
+        controller.searchBar.placeholder = "Search for a Movie or a Tv show"
+        controller.searchBar.searchBarStyle = .minimal
+        return controller
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(discoverTable)
+        discoverTable.delegate = self
+        discoverTable.dataSource = self
+        navigationController?.navigationBar.tintColor = .white  
+        navigationItem.searchController = searchController
+        
+        searchController.searchResultsUpdater = self
+        
+        fetchDiscoverMovies()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        discoverTable.frame = view.bounds
+    }
+    
+    private func fetchDiscoverMovies() {
+        APICaller.shared.getDiscoverMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.discoverTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+
+}
+
+// MARK: - table view delegate methods
+extension SearchViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - table view datasource methods
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+            return UITableViewCell()
+        }
+        let model = titles[indexPath.row]
+        cell.configure(with: TitleViewModel(titleName: model.original_title ?? model.original_name ?? "Unknown*",
+                                            posterURL: model.poster_path ?? "" ))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150 
+    }
+}
+
+
+// MARK: - search result protocol
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        
+        guard let query = searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty,
+              query.trimmingCharacters(in: .whitespaces).count >= 3,
+              let resultsController = searchController.searchResultsController as? SearchResultsViewController else {
+            return
+        }
+        
+        // burada delegate vs kullanacaktı ama eklemediği için kullanamadı :)
+        APICaller.shared.search(with: query) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let titles):
+                    resultsController.titles = titles
+                    resultsController.searchResultsCollectionView.reloadData()
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
---
+++ netflix-clone-ios-uikit/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,63 @@
+//
+//  SearchResultsViewController.swift
+//  netflix-clone-ios-uikit
+//
+//  Created by Samet Koyuncu on 15.10.2022.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+    
+    public var titles: [Title] = []
+    
+    public let searchResultsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width / 3 - 10, height: 200)
+        layout.minimumInteritemSpacing = 0
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(TitleCollectionViewCell.self, forCellWithReuseIdentifier: TitleCollectionViewCell.identifier)
+        return collectionView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        view.addSubview(searchResultsCollectionView)
+        
+        searchResultsCollectionView.delegate = self
+        searchResultsCollectionView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        searchResultsCollectionView.frame = view.bounds
+    }
+
+}
+
+// MARK: - collection view delegate methods
+extension SearchResultsViewController: UICollectionViewDelegate {
+    
+}
+
+// MARK: - collection view datasource methods
+extension SearchResultsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TitleCollectionViewCell.identifier, for: indexPath) as? TitleCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let model = titles[indexPath.row]
+        cell.configure(with: model.poster_path ?? "")
+        
+        return cell
+    }
+    
+    
+}
