6d05c152995b8f0f1998fca607e07810b86efcef
Samet Koyuncu <sametkoyuncu@live.com>
2022-10-16 14:10:02 +0300

    Add random image for hero section

--- netflix-clone-ios-uikit/Controllers/Core/HomeViewController.swift
+++ netflix-clone-ios-uikit/Controllers/Core/HomeViewController.swift
@@ -17,6 +17,9 @@ enum Sections: Int {
 
 class HomeViewController: UIViewController {
     
+    private var randomTrendingMovie: Title?
+    private var headerView: HeroHeaderUIView?
+    
     let sectionTitles: [String] = ["Trending Movies", "Trending Tv", "Popular", "Upcoming Movies", "Top Rated"]
     
     private let homeFeedTable: UITableView = {
@@ -35,9 +38,24 @@ class HomeViewController: UIViewController {
         homeFeedTable.dataSource = self
         
         configureNavbar()
+        configureHeroHeaderView()
         
-        let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+        headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
+    }
+    
+    private func configureHeroHeaderView() {
+        APICaller.shared.getTrendingMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                let selectedTitle = titles.randomElement()
+                self?.randomTrendingMovie = selectedTitle
+                self?.headerView?.configure(with: TitleViewModel(titleName: selectedTitle?.original_title ?? "",
+                                                                 posterURL: selectedTitle?.poster_path ?? ""  ))
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
     private func configureNavbar() {
--- netflix-clone-ios-uikit/Views/HeroHeaderUIView.swift
+++ netflix-clone-ios-uikit/Views/HeroHeaderUIView.swift
@@ -81,4 +81,12 @@ class HeroHeaderUIView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    public func configure(with model: TitleViewModel) {
+        guard let url = URL(string: "https://image.tmdb.org/t/p/w500\(model.posterURL )") else {
+            return
+        }
+        
+        heroImageView.sd_setImage(with: url, completed: nil )
+    }
+    
 }
